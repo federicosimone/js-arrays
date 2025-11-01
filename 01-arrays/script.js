e13448a6a6da46dae0fa9ba83012fa231345fe20
federicosimone <federico.simone15@gmail.com>
2025-11-01 17:08:06 +0100

    2nd and 3rd

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,15 +8,19 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher) ;
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+  console.log(teachers);
+  console.log(teachers[4] = "Patrick") ;
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher) ;
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
